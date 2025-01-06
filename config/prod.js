f2398500ae5dad4fbe673d6432a903b5912518ad
Gal Ben David <gal.benda3@gmail.com>
2025-01-06 16:46:06 +0200

    Fix dbName in prod file

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
     dbURL: 'mongodb+srv://gal:567@cluster0.ejxmt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    dbName: 'tester_db',
+    dbName: 'misterToy_db',
 }
